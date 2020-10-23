eb9e055774462c3d6c23e4932d5fb3988040555b
Gohikeco1 <gohikeco1@gmail.com>
2020-10-22 23:29:20 -0500

    set up routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const pageStyle: React.CSSProperties = {
+	height: "100vh",
+	width: "100vw",
+};
+
+const pageOneStyles: React.CSSProperties = {
+	background: "blue",
+};
+const pageTwoStyles: React.CSSProperties = {
+	background: "red",
+};
+
+const App = () => {
+	return (
+		<Router>
+			<div>
+				<Link to="/">Page 1</Link>
+				<Link to="/page2">Page 2</Link>
+				<Link to="/page22">Page 22</Link>
+			</div>
+			<Switch>
+				<Route path="/page2">
+					<Page styles={{ ...pageTwoStyles, ...pageStyle }}>page 2</Page>
+				</Route>
+				<Route path="/">
+					<Page styles={{ ...pageOneStyles, ...pageStyle }}>page 1</Page>
+				</Route>
+				<Route path="/page22">
+					<Page styles={{ ...pageTwoStyles, ...pageStyle }}>page 22</Page>
+				</Route>
+			</Switch>
+		</Router>
+	);
+};
 
 export default App;
+
+interface PageProps {
+	styles: React.CSSProperties;
+}
+
+const Page: React.FC<PageProps> = ({ children, styles }) => {
+	return <div style={styles}>{children}</div>;
+};
